65d354a57bdd6d93cfa384911f1ea43dda86fef1
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-13 00:33:03 -0300

    SQL, projection, get game by list

--- src/main/java/com/rodrigodev/dslist/services/GameService.java
+++ src/main/java/com/rodrigodev/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rodrigodev.dslist.dto.GameDTO;
 import com.rodrigodev.dslist.dto.GameMinDTO;
 import com.rodrigodev.dslist.entities.Game;
+import com.rodrigodev.dslist.projection.GameMinProjection;
 import com.rodrigodev.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
