276c86e2128a61b0b5794c4fc9bd41d640c1bb7e
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-07-12 23:36:17 -0300

    Get lists

---
+++ src/main/java/com/rodrigodev/dslist/dto/GameListDTO.java
@@ -0,0 +1,28 @@
+package com.rodrigodev.dslist.dto;
+
+import com.rodrigodev.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+	
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+	
+}
